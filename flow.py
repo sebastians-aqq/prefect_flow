6718f70d7fd2e6243af64e7af5251c4b1df65050
sebastians-aqq <sebastian.sobiechPL@gmail.com>
2026-06-17 23:50:01 +0200

    Create flow.py

---
+++ flow.py
@@ -0,0 +1,10 @@
+from prefect import flow
+
+@flow(log_prints=True)
+def find_wifi(name: str = "Spring"):
+    print(f"Wifi {name}!")
+    print(f"G2 ON!")
+    print(f"G5 ON!")
+
+if __name__ == "__main__":
+    hello()
